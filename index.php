58f8204b11c402b45e4688d9f1f2a6f5c010bac9
tleal1 <sr.leal__@hotmail.com>
2022-01-28 18:18:05 -0300

    first commit

---
+++ index.php
@@ -0,0 +1,39 @@
+
+
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Cadastro</title>
+    <link rel="stylesheet" href="index.css">
+    <!--Thiago Leal Miranda-->
+</head>
+<body>
+    <div id="formulário">
+        <form action="">
+            <h2 class="title">Faça Seu Cadastro</h2>
+
+            <label for="Username">Nome</label>
+            <div class="input">
+                <input id="username" name="username" placeholder="Username" type="text">
+            </div>
+
+            <label for="email">Email</label>
+            <div class="input">
+                <input id="email" name="email" placeholder="Email" type="text">
+            </div>
+
+            <label for="senha">Senha</label>
+            <div class="input">
+                <input id="senha" name="senha" placeholder="Password" type="password">
+            </div>
+
+            <div id="botão">
+                <button type="submit"><a href="#">Cadastrar</a></button>
+            </div>
+        </form>
+    </div>
+</body>
+</html>
